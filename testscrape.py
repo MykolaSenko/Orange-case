1730a805f9f2d87885bfed8a1e3fb8634ce01ea2
Bo Cao <bo_cao@yahoo.com>
2023-10-02 15:23:48 +0200

    firt scrap done

--- testscrape.py
+++ testscrape.py
@@ -2,3 +2,6 @@ from bs4 import BeautifulSoup
 import requests
 import json
 from requests import Session
+
+def scraping_packs():
+    https://www2.telenet.be/residential/nl/producten/internet-mobiel/
