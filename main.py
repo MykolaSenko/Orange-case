4ea6de7cd60e82a7c44e7ea7fa36205b8a9b809a
Henrique Rauen <henrique_rauen@yahoo.com.br>
2023-10-11 07:39:37 +0200

    Main updates and remove useless stuff

--- main.py
+++ main.py
@@ -5,16 +5,27 @@ import src.scrape_urls as su
 import src.scrape_gadgets as sg
 import src.Scraper as scraper
 import structlog
+import logging
 import os
 import pandas as pd
 
+structlog.configure(
+    wrapper_class=structlog.make_filtering_bound_logger(logging.WARNING),
+    processors=[
+            structlog.processors.add_log_level,
+            structlog.processors.TimeStamper(fmt="%Y-%m-%d %H:%M:%S",utc=True),
+            structlog.stdlib.PositionalArgumentsFormatter(),
+            structlog.processors.JSONRenderer()],
+    logger_factory=structlog.WriteLoggerFactory(
+        file=Path(f"logs/{time.ctime()}").with_suffix(".log").open("wt"))
+)
 logger=structlog.get_logger()
 start = time.perf_counter()
 #Scrape providers
-# providers = ['telenet_promo','telenet_packs']
-# for provider in providers:
-#     scrp = scraper.Scraper(provider, logger)
-#     scrp.run()
+providers = ['telenet_promo','telenet_packs']
+for provider in providers:
+    scrp = scraper.Scraper(provider, logger)
+    scrp.run()
 #Scrape gadgets urls for telenet and write to CSV so they can be different tasks on airflow
 category_urls=su.get_category_links("https://www2.telenet.be/residential/nl/toestellen/?intcmp=ToestNav", logger)
 all_devices_urls = []
